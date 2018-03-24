a3bd1fd7e1c06dfd1da1551f0861357504ff809d
Савченко Алина <merwood@yandex.ru>
2018-03-24 19:23:20 +0500

    Labs

---
+++ Lab 1/Lab 1.cpp
@@ -0,0 +1,57 @@
+﻿#include<iostream>
+#include <string>  
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	int s = 0;
+	cout << "Введите сумму в копейках:" << endl;
+	cin >> s;
+
+	cout << "Итого: ";
+	int r = 0, k = 0, w = 0, h = 0;
+	r = s / 100;
+	k = s % 100;
+	w = r / 10;
+	h = k / 10; 
+
+	//ОКОНЧАНИЯ
+	string oneR("ль");
+	string someR("ля");
+	string moreR("лей");
+	string oneK("йка");
+	string someK("йки");
+	string moreK("еек");
+
+	if (s == 0) cout << "Ничего не куплено!" << endl;
+	else if (s > 100)
+	{
+		if (k == 0) 
+		{
+			cout << r << " руб";
+			if (w == 1) cout << oneR << endl;
+			if ((w > 1) && (w < 5)) cout << someR  <<endl;
+			if (w >= 5) cout << moreR << endl;
+		}
+		else 
+		{
+			cout << r << " руб";
+			if (w == 1) cout << oneR << endl;
+			if ((w > 1) && (w < 5)) cout << someR << endl;
+			if (w >= 5) cout << moreR << endl;
+			cout << k << " коп";
+			if (h == 1) cout << oneK << endl;
+			if ((h > 1) && (w < 5)) cout << someK << endl;
+			if (h >= 5) cout << moreK << endl;
+		}
+	}
+	else if (s < 100)
+	{
+		cout << r << " руб";
+		if (w == 1) cout << oneR << endl;
+		if ((w > 1) && (w < 5)) cout << someR << endl;
+		if (w >= 5) cout << moreR << endl;
+	}
+		system ("pause");
+		return 0;
+}
---
+++ Lab 2/Lab 2-2.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cstdlib>
+#include <time.h>
+using namespace std;
+int main ()
+{
+	setlocale(0, "");
+	int a;
+	srand(time(0));
+	a = rand()% 1000 + 1;
+	cout << "(Заданное число: " << a << ")" << endl;
+	int b;
+	for (int i = 1; i < 11; i++)
+	{
+		cout << "Попытка №" << i << endl;
+		cout << "Введите число: ";
+		cin >> b;
+		if (a != b)
+		{
+			if (a > b) cout << "Больше" << endl << "___________________" << endl << endl;
+			if (a < b) cout << "Меньше" << endl << "___________________" << endl << endl;
+			if (i == 10) cout << endl << "Вы не отгадали число!Попробуйте в другой раз" << endl << "Загаданное число: " << a << endl;
+		}
+		else if (a == b)
+		{
+			cout << endl << "Вы отгадали число!Браво!Вы отгадали число с " << i << " - й попытки" << endl;
+			break;
+		}
+	}
+	system("pause");
+	return 0;
+}
