84005f16c97c6915bf23f6e733ec8f76b6390ae4
Савченко Алина <merwood@yandex.ru>
2018-03-31 10:59:25 +0500

    labs

---
+++ Lab 2/Lab 2-1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	cout << "¬ведите число: ";
+	int a;
+	cin >> a;
+	cout << a << " = ";
+	int b = 2;
+	 
+	while (a > 1)
+	{
+		for (b = 2; b <= a; b++)
+		{
+			if (a%b == 0)
+			{
+				cout << b << " * ";				
+				a = a / b;
+				break;
+			}
+		}
+	}
+
+	cout << "\b\b" << " " << endl;
+	system("pause");
+	return 0;
+}
