2e42fe1b2bc68e0e0cd953297202c301e6f0aa22
Савченко Алина <31769352+branfield@users.noreply.github.com>
2018-04-21 08:40:21 +0500

    через ооп

--- Lab 1/Lab 1.cpp
+++ Lab 1/Lab 1.cpp
@@ -1,56 +1,61 @@
 #include<iostream>
-#include <string>  
 using namespace std;
+
+int sum = 0, rub = 0, kop = 0;
+
+class Money
+{
+public:
+	int rubLastNum = 0, kopLastNum = 0;
+	void computation()
+	{
+		rub = sum / 100;
+		kop = sum % 100;
+		rubLastNum = rub % 10;
+		kopLastNum = kop % 10;
+	}
+
+	void rubles()
+	{
+		if (rubLastNum == 1) cout << "рубль" << endl;
+		if ((rubLastNum > 1) && (rubLastNum < 5)) cout << "рубля" << endl;
+		if ((rubLastNum >= 5) && (rubLastNum == 0)) cout << "рублей" << endl;
+	}
+
+	void kopeck()
+	{
+		if (kopLastNum == 1) cout << "копейка" << endl;
+		if ((kopLastNum > 1) && (kopLastNum < 5)) cout << "копейки" << endl;
+		if ((kopLastNum >= 5) && (kopLastNum == 0)) cout << "копеек" << endl;
+	}
+};
+
 int main()
 {
 	setlocale(0, "");
-	int sum = 0;
+	Money program;
 	cout << "Введите сумму в копейках:" << endl;
 	cin >> sum;
+	program.computation;
 
 	cout << "Итого: ";
-	int rub = 0, kop = 0, rubLastNum = 0, kopLastNum = 0;
-	rub = sum / 100;
-	kop = sum % 100;
-	rubLastNum = rub / 10;
-	kopLastNum = kop / 10;
-
-	//ОКОНЧАНИЯ
-	string oneRub("ль");
-	string fewRub("ля");
-	string lotRub("лей");
-	string oneKop("ейка");
-	string fewKop("ейки");
-	string lotKop("еек");
 
 	if (sum == 0) cout << "Ничего не куплено!" << endl;
 	else if (sum > 100)
 	{
 		if (kop == 0)
 		{
-			cout << rub << " руб";
-			if (rubLastNum == 1) cout << oneRub << endl;
-			if ((rubLastNum > 1) && (rubLastNum < 5)) cout << fewRub << endl;
-			if ((rubLastNum >= 5) && (rubLastNum == 0)) cout << lotRub << endl;
+			program.rubles();
 		}
 		else
 		{
-			cout << rub << " руб";
-			if (rubLastNum == 1) cout << oneRub << endl;
-			if ((rubLastNum > 1) && (rubLastNum < 5)) cout << fewRub << endl;
-			if ((rubLastNum >= 5) && (rubLastNum == 0)) cout << lotRub << endl;
-			cout << kop << " коп";
-			if (kopLastNum == 1) cout << oneKop << endl;
-			if ((kopLastNum > 1) && (kopLastNum < 5)) cout << fewKop << endl;
-			if ((kopLastNum >= 5) && (kopLastNum == 0)) cout << lotKop << endl;
+			program.rubles();
+			program.kopeck();
 		}
 	}
 	else if (sum < 100)
 	{
-		cout << rub << " руб";
-		if (rubLastNum == 1) cout << oneRub << endl;
-		if ((rubLastNum > 1) && (rubLastNum < 5)) cout << fewRub << endl;
-		if (rubLastNum >= 5) cout << lotRub << endl;
+		program.rubles();
 	}
 	system("pause");
 	return 0;
