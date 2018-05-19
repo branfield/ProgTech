e766e3fedf95404500a8e084caf0b647ad96d0e6
Савченко Алина <31769352+branfield@users.noreply.github.com>
2018-05-19 08:16:41 +0500

    Update Lab 1.cpp

--- Lab 1/Lab 1.cpp
+++ Lab 1/Lab 1.cpp
@@ -1,70 +1,41 @@
 #include<iostream>
-#include <windows.h>
 using namespace std;
-
-int sum = 0, rub = 0, kop = 0, rubLastNum = 0, kopLastNum = 0;
-
-void computation()
-{
-		rub = sum / 100;
-		kop = sum % 100;
-		rubLastNum = rub % 10;
-		kopLastNum = kop % 10;
-}
-
-void rubles()
-{
-		if (rubLastNum == 1) cout << rub << " рубль" << endl;
-		if ((rubLastNum > 1) && (rubLastNum <=4)) cout << rub << " рубля" << endl;
-		if ((rubLastNum >= 5) && (rubLastNum == 0)) cout << rub << " рублей" << endl;
-}
-
-void kopeck()
-{
-		if (kopLastNum == 1) cout << kop << " копейка" << endl;
-		if ((kopLastNum > 1) && (kopLastNum <=4)) cout << kop << " копейки" << endl;
-		if ((kopLastNum >= 5) && (kopLastNum == 0)) cout << kop << " копеек" << endl;
-}
-
 int main()
 {
 	setlocale(0, "");
-	cout << "Введите сумму в копейках:" << endl;
+	int sum = 0;
+	cout << "Введите сумму в копейках: " << endl;
 	cin >> sum;
-	computation();
-
 	cout << "Итого: ";
+	if (sum == 0)
+		cout << "Ничего не куплено!" << endl;
+	else
+	{
+		int kop=0;
+		int rub=0;
+		rub = sum / 100;
+		kop = sum % 100;
+		if (rub != 0)
+		{
+			if (rub == 1 || rub % 10 == 1)
+				cout << rub << " рубль ";
+			else if (rub > 4 || (rub % 10 == 0) || ((rub%100>10)&&(rub%100<20)))
+				cout << rub << " рублей ";
+			else if (rub <= 4 || rub%10==2 ||  rub % 10 == 3 || rub % 10 == 4)
+				cout << rub << " рубля ";
+		}
+		if (kop != 0)
+		{
+			if (kop == 1 || kop % 10 == 1)
+				cout << kop << " копейка ";
+			else if (kop > 4 || (kop % 10 == 0) || ((kop % 100>10) && (kop % 100<20)))
+				cout << kop << " копеек ";
+			else if ( kop <= 4 || kop % 10 == 2 || kop % 10 == 3 || kop % 10 == 4)
+				cout << kop << " копейки ";
+		}
 
-	if (sum == 0) cout << "Ничего не куплено!" << endl;
-	else if (sum > 100)
-	{
-		if (kop == 0)
-		{
-			rub = sum;
-			rubles();
-		}
-		else
-		{
-			if (((rub % 100) > 10) && ((rub % 100) < 20))
-			{
-				cout << rub << " рублей" << endl;
-			}
-			if (((kop % 100) > 10) && ((kop % 100) < 20))
-			{
-				cout << kop << " копеек" << endl;
-			}
-			else if (((rub % 100) < 10) && ((rub % 100) > 20) && (((kop % 100) < 10) && ((kop % 100) > 20)))
-			{
-				rubles();
-				kopeck();
-			}
-		}
 	}
-	else if (sum <= 100)
-	{
-		sum *= 100;
-		computation();
-		rubles();
-	}
+	cout << endl;
 	system("pause");
 	return 0;
+}
